067ab9652b645c3d6659b225c032f67aa06d21f5
lokeshdangii <lokeshdangi1045@gmail.com>
2023-11-07 21:15:43 +0530

    scrip to scrap jeans data from Tata Neu

---
+++ jeans/jeans.py
@@ -0,0 +1,44 @@
+import subprocess
+
+# # path for the bash file
+# bash_script_path = '/home/lokesh/Desktop/Decode/WebScrapping/jeans/jeans.sh'  
+# # running the bash file
+# subprocess.run(['bash', bash_script_path])
+
+# retving the  name and price form the file 
+with open('jeans_curl.txt', 'r') as file:
+
+    price_data = []
+    name_data = []
+
+    for line in file:
+
+        if "name" in line:
+            name_data.append(line.strip())
+        if "priceINR" in line:
+            price_data.append(line.strip())        
+
+# length
+print(len(price_data))
+print(len(name_data))
+
+# writting the name and price to the file
+with open('jeans_data.txt', 'w') as data_file:
+    for name, price in zip(name_data, price_data):
+        data_file.write(name  +'\n' + price  + '\n\n')
+
+
+
+
+# 
+    # MRP_price = []
+    # discount_percent = []
+    # brand_data = []
+
+
+# if "brandName" in line:
+        #     brand_data.append(line.strip())
+        # if "mrpINR" in line:
+        #     MRP_price.append(line.strip())
+        # if "discountPercent" in line:
+        #     discount_percent.append(line.strip())
